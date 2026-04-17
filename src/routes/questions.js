5d6fba9d1dd267b3799ba1eb5bc95d4929377753
Sonja Karvonen <sonja.e.karvonen@gmail.com>
2026-04-17 13:17:31 +0300

    Adding PRISMA

--- src/routes/questions.js
+++ src/routes/questions.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
-const questions = require("../data/questions");
+const prisma = require("../lib/prisma");
 
 // GET /posts 
 // List all posts
@@ -20,19 +19,24 @@ router.get("/", (req, res) => {
   });  
 
   // GET /posts/:postId
-router.get("/:questionId", (req, res) => {
+router.get("/:questionId", async (req, res) => {
     const questionId = Number(req.params.questionId);
   
-    const questions = questions.find((q) => q.id === questionId);
-  
+    const questions = await prisma.question.findUnique({
+      where: { id: questionId},
+      include: { keywords: true},
+    });
+     
+
     if (!questions) {
-      return res.status(404).json({ message: "Post not found" });
+      return res.status(404).json({ 
+        message: "Post not found" });
     }
   
     res.json(questions);
   });  
 
- router.post("/", (req, res) => {
+ router.post("/",async  (req, res) => {
     const { title, date, content, keywords } = req.body;
   
     if (!title || !date || !content) {
@@ -40,27 +44,59 @@ router.get("/:questionId", (req, res) => {
         message: "title, date, and content are required"
       });
     }
-    const maxId = Math.max(...questions.map(q => q.id), 0);
-  
-    const newQuestion = {
-      id: questions.length ? maxId + 1 : 1,
-      title, date, content,
-      keywords: Array.isArray(keywords) ? keywords : []
-    };
-    questions.push(newQuestion);
-    res.status(201).json(newQuestion);
+    
+    const keywordsArray = Array.isArray(keywords) ? keywords : [];
+
+    const newQuestion = await prisma.question.create({
+      data: {
+        title, date: new Date(date), content,
+        keywords: {
+          connectOrCreate: keywordsArray.map((kw) => ({
+            where: { name: kw }, create: { name: kw},
+          })), },
+      },
+      include: { keywords: true},
+    });
+    res.status(201).json(formatQuestion(newQuestion));
+
   });
   
   // PUT /posts/:postId
 // Edit a post
-router.put("/:questionId", (req, res) => {
+router.put("/:questionId", async (req, res) => {
   const questionId = Number(req.params.questionId);
   const { title, date, content, keywords } = req.body;
+  const existingQuestion = await prisma.question.findUnique({where : { id: questionId }
+  });
+
+  if (!existingQuestion) {
+    return res.status(404).json({ message: "Question not found "});
+  }
+  if (!title || !date || !content) {
+    return res.status(400).json({message: "title, sate and content are mandatory"
+  });
+  }
+  const keywordsArray = Array.isArray(keywords) ? keywords : [];
+  const updatedQuestion = await prisma.question.update({
+    where: { id: questionId },
+    data: {
+      title, date: newDate(date), content, 
+      keywords: {
+        set: [],
+        connectOrCreate: keywordsArray.map((kw) => ({
+          where: {name: kw},
+          create: {name: kw},
+        })),
+     },
+    },
+
+    include: { keywords: true},
+  });
 
   const question = questions.find((q) => q.id === questionId);
 
   if (!question) {
-    return res.status(404).json({ message: "Post not found" });
+    return res.status(404).json({ message: "Question not found" });
   }
 
   if (!title || !date || !content) {
@@ -75,6 +111,26 @@ router.put("/:questionId", (req, res) => {
   question.keywords = Array.isArray(keywords) ? keywords : [];
 
   res.json(question);
+});
+
+router.delete("/:questionId", async (req, res) => {
+  const questionId = Number(req.params.postId);
+
+  const question = await prisma.question.findUnique({
+    where: { id: questionId },
+    include: { keywords: true },
+  });
+
+  if (!question) {
+    return res.status(404).json({ message: "Question not found" });
+  }
+
+  await prisma.question.delete({ where: { id: questionId } });
+
+  res.json({
+    message: "Question deleted successfully",
+    question: formatPost(question),
+  });
 });
 
 
